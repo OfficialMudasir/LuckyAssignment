16bc2aa1d8750e9122caf58db13e6a6b6c93f0e5
wani <itzahmad395@gmail.com>
2021-03-10 18:26:42 +0530

    Add project files.

---
+++ LuckyAssignment/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using LuckyAssignment.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Web;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace LuckyAssignment.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Index(Lucky lucky)
+        {
+            Lucky luck = new Lucky();
+            using(var client = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(luck), Encoding.UTF8, "application/json");
+                using (var response = await client.PostAsync("localhost:44328/api/Lucky", content))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    luck = JsonConvert.DeserializeObject<Lucky>(apiResponse);
+                }
+            }
+            
+            return View(luck);
+        }
+    }
+}
---
+++ LuckyAssignment/Controllers/LuckyController.cs
@@ -0,0 +1,62 @@
+﻿using LuckyAssignment.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LuckyAssignment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LuckyController : ControllerBase
+    {
+        // GET: api/<LuckyController>
+        [HttpGet]
+        public ActionResult Get(Lucky lucky)
+        {
+            //these var's are just for test purpose
+            var a = 10;
+            var b = 8;
+
+            //var a = lucky.a;
+            //var b = lucky.b;
+
+            var sum = a + b;
+            var sub = a - b;
+            if(a == 8 || b== 8 || sum == 8 || sub == 8)
+            {
+                return Ok(true);
+            }
+            else if(a == 0 || b == 0)
+            {
+                return BadRequest("Value must not be Zero(0)");
+            }
+            else
+            {
+                return Ok(false);
+            }
+            
+        }
+
+        
+        [HttpPost]
+        public ActionResult Post(Lucky lucky)
+        {
+            
+            if(lucky.a == 0 || lucky.b == 0)
+            {
+                return BadRequest("Value must not be zero(0)");
+            }
+            else
+            {
+                return Ok(lucky.result);
+            }
+            
+        }
+           
+        
+    }
+}
